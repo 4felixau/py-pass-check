a04714478d4031ff889bd14e1df7f18b3dc0f510
secops-expert-50 <4@felixau.in>
2026-07-25 15:05:53 +0530

    Implement features in strength.py - Version 5

--- strength.py
+++ strength.py
@@ -4,3 +4,7 @@ def check_strength(password):
     has_upper = any(c.isupper() for c in password)
     has_lower = any(c.islower() for c in password)
     has_digit = any(c.isdigit() for c in password)
+    length_ok = len(password) >= 8
+    
+    score = sum([has_upper, has_lower, has_digit, length_ok])
+    
