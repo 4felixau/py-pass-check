0eb0e768629c84ab85ab86a06fe6a50d9e3bd53c
secops-expert-50 <4@felixau.in>
2026-07-25 15:05:52 +0530

    Implement features in strength.py - Version 4

--- strength.py
+++ strength.py
@@ -1,3 +1,6 @@
 # Password Strength Checker in Python
 
 def check_strength(password):
+    has_upper = any(c.isupper() for c in password)
+    has_lower = any(c.islower() for c in password)
+    has_digit = any(c.isdigit() for c in password)
